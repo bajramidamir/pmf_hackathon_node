87ab89975ae60efbb588cc733407e77367f3522b
Damir Bajrami <bajrami.damir@gmail.com>
2024-01-12 09:39:39 +0100

    nesto

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const ejsLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const session = require('express-session'); 
+
+// routes
+const loginRouter = require('./routes/loginRouter');
+
+// middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'secret1512',
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(cookieParser());
+app.use(ejsLayouts);
+app.set('layout', false);
+
+// view engine
+app.set('view engine', 'ejs');
+
+
+
+
+app.use('/', loginRouter);
+
+
+
+
+
+app.listen(3000);

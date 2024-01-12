a03ef6629bbcce08206cd300594c36d51c451c75
ArminVejzovic <29armin.vejzovic@gmail.com>
2024-01-12 10:32:57 +0100

    dodao endpointe

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -4,5 +4,12 @@ const userController = require('../controllers/userController');
 router.get('/', (req, res) => {
     res.render('loginSignup');
 });
+
+router.get('/register', (req, res) => {
+    res.render('registerSingup');
+})
+
 router.post('/login', userController.loginUser)
+router.post('/register', userController.createUser);
+
 module.exports = router;
